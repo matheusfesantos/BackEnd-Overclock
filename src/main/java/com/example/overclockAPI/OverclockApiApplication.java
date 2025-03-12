6017589b51977256c114ca0d8f89c861a55429bb
matheusfesantos <matheusfgsantos31@gmail.com>
2025-03-12 16:02:50 -0300

    Adicionado base do Projeto

---
+++ src/main/java/com/example/overclockAPI/OverclockApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.overclockAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverclockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverclockApiApplication.class, args);
+	}
+
+}
