4876313cb992dfd1fd330f24e3debab78b84ca31
matheusfesantos <matheusfgsantos31@gmail.com>
2025-03-12 19:59:11 -0300

    Adicionado PostMapping de novos Usuarios ao controler da aplicação

--- src/main/java/com/example/overclockAPI/controlers/UsuariosControler.java
+++ src/main/java/com/example/overclockAPI/controlers/UsuariosControler.java
@@ -1,12 +1,12 @@
 package com.example.overclockAPI.controlers;
 
 import com.example.overclockAPI.entitys.Usuario;
+import com.example.overclockAPI.exception.ValorNuloException;
 import com.example.overclockAPI.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +22,24 @@ public class UsuariosControler {
         List<Usuario> usuarios = usuarioService.listarTodos();
         return ResponseEntity.ok(usuarios);
     }
+
+    /*
+    @GetMapping({"id"})
+    public ResponseEntity<Usuario> getUsuarioById(@RequestBody Usuario id){
+    }
+     */
+
+    @PostMapping("/adicionar")
+    @ResponseStatus(HttpStatus.ACCEPTED)//INFORMAR SE FOI SUCEDIDA
+    public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario){
+        usuario = usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.ok(usuario);
+    }
+
+   /*
+    @DeleteMapping("/delete")
+    public ResponseEntity<Usuario> deleteUsuario(@RequestBody Usuario usuario){
+
+    }
+    */
 }
