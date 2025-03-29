82dbd5fce37cda723f1e255863dea55bf92cbabc
matheusfesantos <matheusfgsantos31@gmail.com>
2025-03-28 21:50:09 -0300

    feat(movimentacoes): Criando endpoint para a tabela movimentações - Criando Repositorio para extender as funções da JPA em java - Entity para interagir com o banco de dados e a Tabela Movimentações - Criação de um Enum para ler os tipo de movimentações 'entrada', 'saida' - Referenciando ID_pecas a sua tabela original - Inserção de data da movimentação automatica referente a data do POST

---
+++ src/main/java/com/example/overclockAPI/repository/MovimentacoesRepository.java
@@ -0,0 +1,7 @@
+package com.example.overclockAPI.repository;
+
+import com.example.overclockAPI.entitys.Movimentacoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovimentacoesRepository extends JpaRepository<Movimentacoes, Long> {
+}
